80a7da211d65986ebc048028276e2a6696194ebb
Piyumi Weebadu Arachchige <53691448+PiyumiUoR@users.noreply.github.com>
2024-05-17 17:46:17 +0300

    Delete api directory

--- api/static/constants.py
+++
@@ -1,9 +0,0 @@
-# CUSTOMER_PROFILE = "/profiles/sensor/"
-# BOOKING_PROFILE = "/profiles/measurement/"
-# LINK_RELATIONS_URL = "/api/link-relations/"
-
-MASON = "application/vnd.mason+json"
-LINK_RELATIONS_URL = "/api/link-relations/"
-
-CUSTOMER_PROFILE_URL = "/customers/<customer:customer>/"
-BOOKING_DETAILS_URL = "/bookings/booking/"
